f57e7f4f2e897f03c79494f23953366355947f42
Yassine EZZ <yassinezzaarat24@gmail.com>
2025-09-30 14:11:40 +0000

    daily report

---
+++ models/intermediate/int_campaigns_day.sql
@@ -0,0 +1,4 @@
+SELECT date_date, campaign_name, SUM(ads_cost) AS ads_cost, SUM(impression) AS impression, SUM(click) AS click
+FROM {{ ref('int_campaigns') }}
+GROUP BY date_date, campaign_name
+ORDER BY date_date DESC
